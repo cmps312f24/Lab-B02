bbd1a33c5a3c10acb1213da2c2250a101d4cd788
Abdulahi Hassen <abdala.baaji@gmail.com>
2024-09-12 16:52:06 +0300

    updated

--- Lab Practice/Lab 3-Dart OOP and Lambdas/model/Animal.dart
+++ Lab Practice/Lab 3-Dart OOP and Lambdas/model/Animal.dart
@@ -6,12 +6,85 @@
 // 6. abstract classes
 // 7. interfaces
 // 8. Polymorphism
-class Animal {
-  late String name;
-  late int age;
+abstract class Animal {
+  late String _name;
+  late int _age;
 
-  Animal(String name, int age) {
-    this.name = name;
-    this.age = age;
+  // Animal({name, age}) {
+  //   this._name = name;
+  //   this._age = age;
+  // }
+  // Animal.namedConstructor(this._name, this._age);
+
+  Animal({name, age})
+      : this._name = name,
+        this._age = age;
+  // Animal(this._name, this._age);
+
+  set name(String name) {
+    if (name.length >= 2) {
+      _name = name;
+    } else {
+      throw ('Name must be at least 2 characters');
+    }
+  }
+
+  String get name => _name;
+
+  set age(int age) => this._age = age;
+  int get age => _age;
+
+  void makeSound();
+}
+
+class Lion extends Animal with Jumpable {
+  late int _prideSize;
+
+  Lion({name, age, prideSize}) : super(name: name, age: age) {
+    this.prideSize = prideSize;
+  }
+  set prideSize(int prideSize) => this._prideSize = prideSize;
+  int get prideSize => _prideSize;
+
+  void makeSound() {
+    print('Roar');
+  }
+}
+
+mixin Jumpable {
+  void jump() {
+    print('Jumping ...');
+  }
+}
+mixin Swimmable {
+  void Swim() {
+    print('Swimming ...');
+  }
+}
+
+class Human extends Animal with Jumpable, Swimmable {
+  late String _job;
+
+  Human({name, age, job}) : super(name: name, age: age) {
+    this.job = job;
+  }
+
+  set job(String job) => this._job = job;
+  String get job => this._job;
+
+  @override
+  void makeSound() {
+    print('Hello');
+  }
+}
+
+void main(List<String> args) {
+  Animal human = Human(name: 'John', age: 30, job: 'Software Engineer');
+  Animal lion = Lion(name: 'Simba', age: 5, prideSize: 15);
+
+  List<Animal> animals = [human, lion, animal];
+
+  for (Animal animal in animals) {
+    animal.makeSound();
   }
 }
